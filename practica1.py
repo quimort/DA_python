f1c932daf9beb0c0b1b7690caf8e2cba5a9eda76
quim <quimort97@gmail.com>
2025-02-12 18:29:20 +0100

    add is_recall and author

--- practica1.py
+++ practica1.py
@@ -1,3 +1,5 @@
+# Author: Freddy Vargas; Yoel Carocancha; Quim Ortuño
+
 from operator import itemgetter
 import pandas as pd
 import os
@@ -21,6 +23,7 @@ def precursor_call_id(data):
     data['call_ts_previous'] = data.groupby('customer_id')['call_ts'].shift(1)
     data['call_id_previous'] = data.groupby('customer_id')['call_id'].shift(1)
     mask = inRecallTimeWindow(data['call_ts_previous'], data['call_ts'])
+    data['is_recall'] = mask.map({True: 'Y', False: 'N'})
     data['precursor_call_id'] = data['call_id_previous'].where(mask).astype('Int64')
     data['hours_from_first_call'] = ((data['call_ts']- data['call_ts_previous']).dt.total_seconds() / 3600).where(mask).astype("float64")
     data.drop(columns=['call_ts_previous','call_id_previous'],inplace=True)
@@ -38,11 +41,11 @@ def process_chunk(chunk,prev_last_row, next_first_row,output_file,first_chunk):
 
     chunk = is_precursor(chunk)
     chunk = precursor_call_id(chunk)
-    # Extract first and last rows for next iteration
+    # Extraer primera and ultima filas para cada iteracion
     next_prev_last_row = chunk[['call_id','customer_id','call_ts']].iloc[[-1]]  # Last row for next chunk
-    next_first_row = chunk.iloc[[0]]  # First row for next chunk
+    next_first_row = chunk.iloc[[0]]  # Primera fila para el chunk siguiente
 
-    # Remove first and last row before saving
+    # Eliminar primera y ultima fila antes de guardar
     chunk = chunk.iloc[1:-1]  
 
     chunk.to_csv(output_file, mode='a', index=False, header=first_chunk,quoting=csv.QUOTE_NONNUMERIC)
@@ -58,7 +61,7 @@ def chunking_sort(chunk_size,input_file,output_file,sort_by):
         chunk.to_csv(file_name, index=False)
         file_names.append(file_name)
 
-    # merge the chunks
+    # Unir los chunks
     chunks = [csv.DictReader(open(file_name)) for file_name in file_names]
     with open(output_file, "w",newline='', encoding='utf-8') as outfile:
         field_names = ["call_id", "customer_id","call_ts"]
@@ -75,10 +78,10 @@ def main_by_chunk():
 
     # Esta parte es el codigo para el processamiento de los datos en chunks
     CHUNK_SIZE = 1000  
-    INPUT_FILE = "./calls_without_target.csv/calls_without_target_in.csv"   
-    OUTPUT_FILE = "./calls_without_target.csv/processed_calls.csv"
-    file_names = chunking_sort(CHUNK_SIZE,"./calls_without_target.csv/calls_without_target.csv",INPUT_FILE,['customer_id','call_ts'])
-    # Remove output file if it exists
+    INPUT_FILE = "calls_without_target_in.csv"   
+    OUTPUT_FILE = "processed_calls.csv"
+    file_names = chunking_sort(CHUNK_SIZE,"calls_without_target.csv",INPUT_FILE,['customer_id','call_ts'])
+    # Quitar el archivo de salida output si es que existe
     for file in file_names:
         os.remove(file)
     if os.path.exists(OUTPUT_FILE):
@@ -89,24 +92,25 @@ def main_by_chunk():
 
     with pd.read_csv(INPUT_FILE, chunksize=CHUNK_SIZE, sep=";", names=['call_id','customer_id','call_ts'], header=0) as reader:
         first_chunk = True
-        next_chunk = next(reader, None)  # Read first chunk to preload next_first_row
+        next_chunk = next(reader, None)  # Leo el primer chunk para precargar next_first_row
 
         while next_chunk is not None:
-            # Read ahead to get first row of the next chunk
+            # Leer hacia adelante para obtener primer fila del siguiente chunk
             future_chunk = next(reader, None)  
             if future_chunk is not None:
                 next_first_row = pd.DataFrame([future_chunk.iloc[[0]].values.flatten()],columns=['call_id','customer_id','call_ts'])
 
-            # Process current chunk safely
+            # Procesar el current chunk
             prev_last_row, next_first_row = process_chunk(next_chunk, prev_last_row, next_first_row, OUTPUT_FILE,first_chunk)
 
-            # Move to next chunk
+            # Ir al sigueinte chunk
             next_chunk = future_chunk
             first_chunk = False  
 
+#Proceso para el archivo completo usando pandas y los chunks
 def main():
 
-    path = "./calls_without_target.csv/calls_without_target.csv"
+    path = "calls_without_target.csv"
 
     data = pd.read_csv(path,sep=';')
     data = data.sort_values(by=['customer_id','call_ts'])
